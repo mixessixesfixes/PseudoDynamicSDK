2817509cd8a7d3a14988062bba52500770d4b1c2
Masakazu Suetomo <git@moonxseed.com>
2018-07-18 19:08:44 +0900

    Add PseudoDynamicSDK.framework

---
+++ PseudoDynamicSDK.framework/Headers/PseudoDynamicSDK.h
@@ -0,0 +1,19 @@
+//
+//  PseudoDynamicSDK.h
+//  PseudoDynamicSDK
+//
+//  Created by mxs on 2018/07/18.
+//  Copyright © 2018 AnchorZ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PseudoDynamicSDK.
+FOUNDATION_EXPORT double PseudoDynamicSDKVersionNumber;
+
+//! Project version string for PseudoDynamicSDK.
+FOUNDATION_EXPORT const unsigned char PseudoDynamicSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PseudoDynamicSDK/PublicHeader.h>
+
+
